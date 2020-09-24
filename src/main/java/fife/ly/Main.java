585b2f79bed2de31c39cf3fba5a1342e9c35ce23
lwmfjc <lwmfjc@gmail.com>
2020-09-24 11:15:04 +0800

    '将数据库连接改为在测试类中'

--- src/main/java/fife/ly/Main.java
+++ src/main/java/fife/ly/Main.java
@@ -8,29 +8,11 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/musicdb?useSSL=true&serverTimezone=UTC",
-                    "prospring5","prospring5");
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM SINGER");
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()){
-                System.out.println(resultSet.getString("first_name"));
-            }
-            connection.close();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        /*ApplicationContext applicationContext=
+        ApplicationContext applicationContext=
                 //new AnnotationConfigApplicationContext(Config.class)
                 new ClassPathXmlApplicationContext("classpath:context.xml");
 
         Bed bed=applicationContext.getBean(Bed.class);
-        bed.usePillow();*/
+        bed.usePillow();
     }
 }
--- src/test/java/TestLy.java
+++ src/test/java/TestLy.java
@@ -1,2 +1,27 @@
+import org.junit.Test;
+
+import java.sql.*;
+
 public class TestLy {
+    @Test
+    public void testSimpleDb(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/musicdb?useSSL=true&serverTimezone=UTC",
+                    "prospring5","prospring5");
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM SINGER");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("first_name"));
+            }
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
