7f499e565901aa27b805932f0133811fa81d88f5
lwmfjc <lwmfjc@gmail.com>
2020-09-25 10:43:03 +0800

    '初始化bean之前的三个回调方式'

--- src/main/java/fife/ly/config/ConfigH2.java
+++ src/main/java/fife/ly/config/ConfigH2.java
@@ -1,5 +1,6 @@
 package fife.ly.config;
 
+import fife.ly.entity.Bed;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/fife/ly/entity/Bed.java
+++ src/main/java/fife/ly/entity/Bed.java
@@ -1,11 +1,30 @@
 package fife.ly.entity;
 
-public class Bed {
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+
+public class Bed implements InitializingBean {
     private String name;
     private Pillow pillow;
 
     public String getName() {
         return name;
+    }
+
+    /**注意看context.xml文件,由于使用了注解,需要声明context:annotation-config
+     *
+     */
+    /**
+     * 该方法在bea类被投入使用之前及bean实际初始化之前调用
+     */
+    @PostConstruct
+    public void init1(){
+        System.out.println("init1");
+    }
+
+    public void init2(){
+        System.out.println("init2");
     }
 
     public void setName(String name) {
@@ -26,5 +45,13 @@ public class Bed {
         }else {
             System.out.println("The bed has no pillow!");
         }
+    }
+
+    /**
+     * 在注入依赖项后立即执行
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("init3");
     }
 }
